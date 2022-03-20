aa02c7122ebd5cac1cde4617081b6b6b1f574756
IvanDamNation <ivan.suvalkin@gmail.com>
2022-03-20 03:41:33 +0300

    Add sign in form and page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
+from .views import BaseRegisterView
 
 urlpatterns = [
     path('',
@@ -8,5 +9,8 @@ urlpatterns = [
     path('logout/',
          LogoutView.as_view(template_name='accounts_logout.html'),
          name='logout'),
+    path('register/',
+         BaseRegisterView.as_view(template_name='accounts_register.html'),
+         name='signup'),
 
 ]
